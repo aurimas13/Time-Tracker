95ab0cc509db3ae17d11f075144ae226a14e7dc9
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-09-12 22:28:09 +0300

    Updated file

--- app/models.py
+++ app/models.py
@@ -24,11 +24,11 @@ class Task(db.Model):
     status = db.Column(db.Boolean, default=False)
     description = db.Column(db.String(140))
     estimated_time = db.Column(db.Integer)
-    developer_id = db.Column(db.Integer, db.ForeignKey('task.task_id')) # sitas turi buti assigninamas
+    developer_id = db.Column(db.Integer, db.ForeignKey('developer.id')) # sitas turi buti assigninamas
     iteration = db.Column(db.String(64))
 
     def __repr__(self):
-        return '<Task {} of story id {} with task id {}>'.format(self.story_id, self.task_name, self.task_id)
+        return '<Task {} of story id {} with task id {}>'.format(self.task_id, self.story_id, self.task_id)
 
 
 class TaskActualTimes(db.Model):
@@ -43,7 +43,8 @@ class TaskActualTimes(db.Model):
 class Developer(db.Model):
     """     Developer Schema    """
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    developer_name = db.Column(db.String(64), index=True, unique=True)
+    name = db.Column(db.String(64), unique=True)
+    # Jei yra du jonai reikia sukurti emailus kurie yra unique ir leidzia atskirti developerius Jonus ar panasiai, tada email unique o developer name'ai nebe
 
     def __repr__(self):
-        return '<Developer {} with developer id {}>'.format(self.developer_name, self.id)
+        return '<Developer {} with developer id {}>'.format(self.name, self.id)
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,6 @@
 from flask import render_template, flash, redirect, url_for, request
+from werkzeug.exceptions import BadRequestKeyError
+
 from app import app, db
 from app.forms import LoginForm, StoryForm, TaskForm
 from app.models import Story, Task, Developer, TaskActualTimes
@@ -7,7 +9,6 @@ from itertools import groupby
 from operator import itemgetter
 
 
-@app.route('/', methods=['POST','GET'])
 @app.route('/index', methods=['POST','GET'])
 def index():
     story = {'STORY_NAME': 'Home'}
@@ -33,12 +34,18 @@ def index():
         return render_template('index.html', title='Home', story=story, stories=stories)
 
 
+@app.route('/', methods=['POST','GET'])
 @app.route('/story', methods=['POST', 'GET'])
 def story():
     story = {'STORY_NAME': 'Stories'}
     if request.method == 'GET':
+        try:
+            story_actual_time = request.args['story_actual_time']
+        except BadRequestKeyError:
+            story_actual_time = None
+        task = Task.query.all()
         stories = Story.query.all()
-        return render_template('story.html', title='Tracker', story=story, stories=stories)
+        return render_template('story.html', title='Tracker', task=task, story=story, stories=stories, story_actual_time=story_actual_time)
         # return redirect(url_for('index', story=story))
     # elif request.method == 'POST':
     #     if request.form.get('Create Story') == 'Create Story':
@@ -49,24 +56,19 @@ def story():
 
 @app.route('/story/<id>', methods=['POST', 'GET'])
 def story_id(id):
-    task = {'TASK_NAME': 'Tasks'}
     if request.method == 'GET':
+        story = Story.query.filter_by(id=id).first()
+        # print(story)
         query_result = db.session.query(Task, TaskActualTimes).filter(
             Task.story_id == id).filter(
             Task.task_id == TaskActualTimes.task_id).all()
         tasks = {}
-        # for row in query_result:
-        #     item = row[0].__dict__
-        #     if item['task_id'] in tasks:
-        #         tasks[item['task_id']]['actual_times'].append(row[1].__dict__)
-        #     else:
-        #         tasks[item['task_id']] = row[0].__dict__
-        #         tasks[item['task_id']]['actual_times'] = list(row[1].__dict__)
+        story_actual_time = 0
         for row in query_result:
             task = row[0].__dict__
             actual_time = row[1].__dict__
             key_id = task['task_id']  # pvz, 1, 2, 3
-            print(actual_time)
+            story_id = task['story_id']
             if key_id in tasks:  # pvz, if 1 in tasks
                 if actual_time['actual_time']:
                     # print(actual_time['actual_time'])
@@ -75,19 +77,22 @@ def story_id(id):
             else:
                 tasks[key_id] = task  # pvz, tasks[1] = {'task_id': 1}
                 if actual_time['actual_time']:
-                    # print(actual_time)
-                    tasks[key_id]['actual_times'] = list(actual_time)  # # pvz, tasks[1]['actual_times'] = {'actual_time': 10}
+                    tasks[key_id]['actual_times'] = [actual_time]  # # pvz, tasks[1]['actual_times'] = {'actual_time': 10}
                     tasks[key_id]['actual_times_sum'] = actual_time['actual_time']
                 else:
                     tasks[key_id]['actual_times'] = []
-                # print(tasks[key_id]['actual_times_sum'])
-                # print(actual_time['actual_time'])
+                    tasks[key_id]['actual_times_sum'] = 0
 
+            if story_id in tasks:
+                story_actual_time += tasks[key_id]['actual_times_sum']
+
+        print(story_actual_time)
         tasks = tasks.values()
-        # print(tasks)
-        # print(list(map(itemgetter('actual_times'), tasks)))
-        # print(tuple(d.get('actual_time') for d in tasks))
-        return render_template('task.html', title='Tracker', id=id, task=task, tasks=tasks)
+        return render_template('task.html', title='Tracker', id=id, story=story, tasks=tasks, story_actual_time=story_actual_time)
+
+    # elif request.method == 'POST':
+    #     story = Story.query.all()
+    #     return render_template('story.html', story=story)
     # elif request.method == 'POST':
     #     if request.form.get('Create Task') == 'Create Task':
     #         return 'lala'
@@ -133,17 +138,65 @@ def update_story(id):
     #
     # return render_template('create_story.html', form=form)
 
+@app.route('/create_developer', methods=['POST', 'GET'])
+def create_developer():
+    if request.method == 'POST':
+        developer = request.form
+        add_developer = Developer(name=developer['developer_name'])
+        db.session.add(add_developer)
+        db.session.commit()
+        return redirect(url_for('story'))
+
+    return render_template('create_developer.html', title='Tracker')
+
+@app.route('/developer_summary', methods=['POST', 'GET'])
+def developer_summary():
+    if request.method == 'GET':
+        query_result = db.session.query(Task, TaskActualTimes, Developer).filter(Task.developer_id == Developer.id).filter(
+            Task.task_id == TaskActualTimes.task_id).all()
+        print(query_result)
+        developers = {}
+        for row in query_result:
+            task = row[0].__dict__
+            actual_time = row[1].__dict__
+            developer = row[2].__dict__
+            story_id = task['story_id']
+            dev_id = developer['id']
+            if dev_id in developers:
+                if actual_time['actual_time']:
+                    developers[dev_id]['actual_times_sum'] += actual_time['actual_time']
+                    developers[dev_id]['estimated_time'] = task['estimated_time']
+            else:
+                developers[dev_id] = developer  # pvz, tasks[1] = {'task_id': 1}
+                if actual_time['actual_time']:
+                    developers[dev_id]['actual_times_sum'] = actual_time['actual_time']
+                    developers[dev_id]['estimated_time'] = task['estimated_time']
+                else:
+                    developers[dev_id]['actual_times_sum'] = 0
+                    developers[dev_id]['estimated_time'] = 0
+
+        developers = developers.values()
+        print(developers)
+        # print(developers[dev_id]['actual_times_sum'])
+        # print(developers[dev_id]['estimated_time'])
+        # print(developers)
+        # print(developers[dev_id]["name"])
+        # print(f'Developer {developer["name"]} estimates {task["estimated_time"]} hours to complete the task while it took {actual_time["actual_time"]} hours to finish the task.')\
+        return render_template('developer.html', developers=developers)
+
 
 @app.route('/story/<id>/create_task', methods=['POST', 'GET'])
 def create_task(id):
     if request.method == 'POST':
         task = request.form
+        dev_id = request.form.get('comp_select')
         task_actual = request.form
         add_task = Task(
             task_name=task['task_name'],
             story_id=id,
             status=task['check'] == 'on',
             description=task['task_description'],
+            developer_id=dev_id,
             estimated_time=task['time'],
             iteration=task['iter'])
         # print(add_task)
@@ -158,8 +211,9 @@ def create_task(id):
             db.session.add(time)
         db.session.commit()
         return redirect(url_for('story_id', id=id))
-#
-    return render_template('create_task.html', title='Tracker')
+
+    developers = Developer.query.all()
+    return render_template('create_task.html', title='Tracker', developers=developers)
 
 
 @app.route('/story/<story_id>/update_task/<task_id>', methods=['POST', 'GET'])
@@ -167,13 +221,17 @@ def update_task(story_id, task_id):
     if request.method == 'GET':
         # tasks = Task.query.filter_by(id=task_id)
         task = Task.query.get(task_id)
-        return render_template('update_task.html', title='Tracker', task=task)
+        developers = Developer.query.all()
+        return render_template('update_task.html', title='Tracker', task=task, developers=developers)
 
     elif request.method == 'POST':
-        # return 'kasdkask'
+        # developers = Developer.query.all()
+        dev_id = request.form.get('comp_select')
+        print('This is the developer id ', dev_id)
         item = Task.query.get(task_id)
         item.task_name = request.form['task_name']
         item.story_id = story_id
+        item.developer_id = dev_id
         item.status = 'check' in request.form
         item.description = request.form['task_description']
         item.estimated_time = request.form['time']
