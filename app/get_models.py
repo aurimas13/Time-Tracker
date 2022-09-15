0c23ffb4f4e3ccf04958f7aeb7718b6b0c586a5d
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-09-16 00:13:52 +0300

    Updated README.md

--- app/get_models.py
+++
@@ -1,9 +0,0 @@
-from app.models import Developer, Task, Story, TaskActualTimes
-
-
-def get_task_taskactualtimes_by_ids(db):
-    print(Task, TaskActualTimes)
-    query = db.session.query(Task, TaskActualTimes).filter(
-            Task.story_id == id).filter(
-            Task.task_id == TaskActualTimes.task_id).all()
-    return query
